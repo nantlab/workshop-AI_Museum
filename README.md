5c3defed7de737eb952eb6a039aab1affc173955
Thomas Geissl <thomas.geissl@gmail.com>
2023-04-20 06:25:52 +0200

    added cache

--- README.md
+++ README.md
@@ -7,4 +7,9 @@
 * [demo] anpassung in der demo app
 * [exercise] chatgpt webinterface: anpassung der sprache in einfache sprache (fuer kinder)
 * [demo] anpassung in der demo app
-* [exercise] ideation: weitere anwendungen von llm im musealen kontext
+* [exercise] ideation/discussion: weitere anwendungen von llm im musealen kontext
+
+## further ideas
+* chat bot to explain game logic (onboarding)
+* adding a VUI on top (voice input and output)
+* fine tuning model with custom data
--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,10 @@ import { ChatGPTAPI, ChatGPTUnofficialProxyAPI } from "chatgpt";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import NodeCache from "node-cache"
+import md5 from "md5"
 dotenv.config();
+const cache = new NodeCache();
 
 let api: ChatGPTAPI | null = null;
 if (process.env.OPENAI_API_KEY) {
@@ -15,6 +18,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/translateLanguage", async function (req, res) {
+  console.log("translating ...", req.body)
+  const hash = md5(JSON.stringify(req.body))
+  if(cache.has( hash )){
+    const cachedResponse:any = cache.get(hash)
+    res.send(cachedResponse);
+    return 
+  }
   let prompt = `translate the following text into ${req.body.language} language. `;
   if(req.body.simplify === true){
     prompt = `${prompt} and simplify it, in a way that it is understandable by a 10 year old pupil. `
@@ -23,6 +33,7 @@ app.post("/translateLanguage", async function (req, res) {
     let response = await api.sendMessage(
       `${prompt} here is the input text:\n\n ${req.body.input}`
     );
+    cache.set(hash, JSON.stringify(response))
     res.send(response);
   }
 });
